3823b77c46a177b247f0620c669094ebea07029e
smarshall5 <smarshall5@my.wctc.edu>
2019-09-17 09:24:35 -0500

    Add project files.

---
+++ wwwroot/js/client.js
@@ -0,0 +1,26 @@
+﻿$(function(){
+    var toast = new Audio('media/toast.wav');
+
+    $('.code').on('click', function(e) {
+        e.preventDefault();
+        toast.pause();
+        toast.currentTime = 0;
+        toast.play();
+        $('#toast').toast({ autohide: false }).toast('show');
+    });
+    $(document).on('keyup', function (e) {
+        if (e.keyCode == 27) {
+            $('#toast').toast('hide');
+        }
+    });
+    $('#seasoningCode').data({ 'Header': "Anton's Seasoning", 'code': "Anton35" });
+    $('#notseasoningCode').data({ 'Header': "Not seasoning", 'code': "NoSeason45" });
+    $('#stealCode').data({ 'Header': "Stolen Goods", 'code': "Stolen50" });
+
+    $('.code').on('click', function () {
+        $('.toast').toast({ autohide: false }).toast('show');
+        $('.ProductNameHead').text($(this).data('Header'));
+        $('.toast-code').text("Discount code: " + $(this).data('code'));
+    })
+
+});
